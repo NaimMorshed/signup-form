8c4f05787917bc7cf60e783d05681a1c31a49e7b
Naim Morshed <morshed.naim13@gmail.com>
2021-06-26 19:00:15 +0600

    phoneAuth implemented

--- src/components/Auth/PhoneSign.js
+++ src/components/Auth/PhoneSign.js
@@ -7,38 +7,29 @@ const PhoneSign = () => {
     const numberRef = useRef(null);
     const verificationRef = useRef(null);
 
-    const submit = event => {
+    const verifyRecaptcha = event => {
         window.recaptchaVerifier = new firebase.auth.RecaptchaVerifier('recaptcha-container', {
             'size': 'visible',
             'callback': (response) => {
                 // reCAPTCHA solved, allow signInWithPhoneNumber.
-                onSignInSubmit();
+                formSubmit();
             }
         });
     }
 
-    const onSignInSubmit = (event) => {
+    const formSubmit = (event) => {
         event.preventDefault();
-        submit();
+        verifyRecaptcha();
 
         const appVerifier = window.recaptchaVerifier;
         firebase.auth().signInWithPhoneNumber(numberRef.current.value, appVerifier)
             .then(confirmationResult => {
                 setVerifyForm(true);
-                // SMS sent. Prompt user to type the code from the message, then sign the
-                // user in with confirmationResult.confirm(code).
                 window.confirmationResult = confirmationResult;
-
                 const code = verificationRef.current.value;
-                confirmationResult.confirm(code).then((result) => {
-                    alert('success');
-                    const user = result.user;
-                    // ...
-                }).catch((error) => {
-                    alert(error.message)
-                    // User couldn't sign in (bad verification code?)
-                    // ...
-                });
+                confirmationResult.confirm(code)
+                .then(() => alert('success'))
+                .catch((error) => alert(error.message));
 
             })
             .catch(error => alert(error.message));
@@ -48,7 +39,7 @@ const PhoneSign = () => {
         <div className="App">
             <main className="App-header login-background">
                 <div className="parent">
-                    <form onSubmit={onSignInSubmit} className="login-form">
+                    <form onSubmit={formSubmit} className="login-form">
                         <div className="input-div">
                             <input ref={numberRef} className="border border-primary" type="tel" placeholder="+8801*********" required />
                         </div>
